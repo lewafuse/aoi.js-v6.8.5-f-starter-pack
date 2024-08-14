e7ff487d7e41865366a69af45ea2580496daf35f
Lewafuse <87063319+lewafuse@users.noreply.github.com>
2024-08-14 12:15:54 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# PLUWZ - AOI.JS v - Example Project
+
+**Bu Proje aoi.js kullanıcılarının daha kolay ve dinamik bir başlangıç yapabilmesi için tasarlanmıştır.**
+
+### [Discord Sunucumuz](https://discord.gg/) | [Youtube Kanalımız](https://youtube.com/@pluwz) | [Instagram Sayfamız](https://www.instagram.com/lewafuse0)
+
+### **Başlamadan önce**
+`.env` dosyasına gelin ve **TOKEN** adında bir **KEY** açın, değerine **bot_tokeniniz** girin.
+
+### Modüller
+```
+npm i aoi.js@6.8.5
+npm i node@20
+npm i 
+```
+
+### **Herşey tamam sayılır, şimdi projemizi aktif hale getirelim.**
+```
+node .
+```
+veya
+```
+node index.js
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+const { AoiClient, LoadCommands } = require("aoi.js");
+//const { AoiVoice, PlayerEvents, PluginName, Cacher, Filter } = require("@akarui/aoi.music");
+
+const keep_alive = require('./keep_alive.js')
+const lwconfig = require('./configs/lwconfig.json')
+
+const bot = new AoiClient({
+  token: process.env['TOKEN'],//lwconfig.token,
+  prefix: "$getVar[prefix]",
+  intents: ["MessageContent", "Guilds", "GuildMembers", "GuildMessages", "GuildBans", "GuildWebhooks", "GuildPresences"],
+  events: ['onMessage',
+    'onJoin',
+    'onLeave',
+    'onGuildJoin',
+    'onGuildLeave',
+    'onInteractionCreate']
+  /*
+      database: {
+      type: "dbdjs.db",
+      db: require('dbdjs.db'),
+      path: "./database/",
+      tables: ['trial']
+      }
+  */
+})
+
+
+/*
+BU PROJE LEWAFUSE TARAFINDAN YENİ BAŞLAYANLAR İÇİN TASARLANMIŞTIR.
+*/
+
+
+//LwLoader
+const loader = new LoadCommands(bot);
+loader.load(bot.cmd, "./komutlar/")
+
+//variables
+bot.variables(require('./configs/lwvariables.js'));
+
+
+
+/*
+const voice = new AoiVoice(
+  LWbot,
+  {
+    cache: {
+      cacheType: "Memory",
+      enabled: true,
+    },
+  },
+  false, //to enable pruneMusic 
+);
+voice.onTrackStart()
+*/
+
+
+
+//------------ REBOOT -----------//
+bot.readyCommand({
+  channel: "",
+  code: `
+$log[========================================]
+$log[Sunucu           || $guildCount]
+$log[Yüklenen Komut   || $commandsCount]
+$log[Bot Ping         || $pingms]
+$log[$userTag[$clientID]   || Bot AKTIF]
+$log[========================================]
+`
+})
+
+
+
+//--------------STATUS--------------//
+bot.status({
+  name: lwconfig.botStatus,
+  type: "PLAYING",
+  status: "online",
+  time: 7
+})
+
+bot.status({
+  name: lwconfig.botStatus1,
+  type: "WATCHING",
+  status: "online",
+  time: 7
+})
+
+bot.status({
+  name: lwconfig.botStatus2,
+  type: "LISTENING",
+  status: "online",
+  time: 7
+})
