d4f90164acd88c75756bb0013fc7cc7c55f8a919
Lewafuse <87063319+lewafuse@users.noreply.github.com>
2024-08-14 12:16:13 +0300

    Add files via upload

---
+++ lwvariables.js
@@ -0,0 +1,13 @@
+module.exports = {
+  prefix: "!",
+  mainserver: "SUNUCU_ID",
+  owners: "DIĞER_BOT_SAHIBI_ID",
+  
+  globalcolor: "2f3136",
+  mavi: "4e6aff",
+  kırmızı: "ff4e4e",
+  sarı: "ffd624",
+  mor: "884eff",
+  pembe: "ff4ef3",
+  turuncu: "ff824e"
+}
